b9a98aaaa66fadadf0b90f872d7e4582d4a12d55
Tatev <tatbejanyan@gmail.com>
2020-01-29 20:35:43 +0400

    Action bar Menu

--- app/src/main/java/com/example/news/adapter/NewsAdapter.java
+++ app/src/main/java/com/example/news/adapter/NewsAdapter.java
@@ -24,6 +24,7 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsHolder> {
     private List<Result> listResultNews;
     private Context mContext;
     private SectionClickListener sectionClickListener;
+    private int position;
 
     public NewsAdapter(Context mContext, SectionClickListener sectionClickListener) {
         this.mContext = mContext;
@@ -48,6 +49,11 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsHolder> {
     public void onBindViewHolder(@NonNull NewsHolder holder, int position) {
         holder.title.setText(listResultNews.get(position).getSectionName());
         holder.description.setText(listResultNews.get(position).getWebTitle());
+        if (listResultNews.get(position).isRedLine()) {
+            holder.redLineView.setVisibility(View.VISIBLE);
+        }else {
+            holder.redLineView.setVisibility(View.INVISIBLE);
+        }
         Picasso.get().load(String.valueOf(listResultNews.get(position).getWebUrl())).into(holder.imgNews);
     }
 
@@ -60,15 +66,23 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsHolder> {
         private TextView title;
         private TextView description;
         private ImageView imgNews;
+        private View redLineView;
 
         NewsHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.txtTitle);
             description = itemView.findViewById(R.id.txtDescription);
             imgNews = itemView.findViewById(R.id.newsImg);
+            redLineView = itemView.findViewById(R.id.redViewLine);
+            if (listResultNews.get(position).isRedLine()) {
+                redLineView.setVisibility(View.VISIBLE);
+            }
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    position = getAdapterPosition();
+                    redLineView.setVisibility(View.VISIBLE);
+                    listResultNews.get(getAdapterPosition()).setRedLine(true);
                     Toast.makeText(mContext, listResultNews.get(getAdapterPosition()).getSectionName(), Toast.LENGTH_LONG).show();
                 }
             });
